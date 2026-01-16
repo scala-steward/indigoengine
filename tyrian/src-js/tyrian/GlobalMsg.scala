c050340d7fd1817e7e3352465a8f368f940c6e27
davesmith00000 <davesmith.00000@gmail.com>
2026-01-16 08:03:29 +0000

    Updated sandbox build links and a stale comment

--- tyrian/src-js/tyrian/GlobalMsg.scala
+++ tyrian/src-js/tyrian/GlobalMsg.scala
@@ -1,6 +1,6 @@
 package tyrian
 
-/** Base trait for all messages in tyrian-next applications. All events, user actions, and system messages must extend
+/** Base trait for all messages in Tyrian applications. All events, user actions, and system messages must extend
   * GlobalMsg to participate in the application's update cycle.
   */
 trait GlobalMsg
