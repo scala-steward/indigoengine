1668ba08ea447f042b30b1809d1341758096a391
davesmith00000 <davesmith.00000@gmail.com>
2026-05-10 09:12:51 +0100

    Slimming down the native sandbox

--- indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/SandboxGame.scala
+++ indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/SandboxGame.scala
@@ -1,19 +1,20 @@
 package com.example.sandbox
 
 import com.example.sandbox.scenes.*
-import example.TestFont
+// import example.TestFont
 import indigo.*
-import indigo.json.Json
-import indigoextras.effectmaterials.LegacyEffects
-import indigoextras.effectmaterials.Refraction
-import indigoextras.subsystems.FPSCounter
+// import indigo.json.Json
+// import indigoextras.effectmaterials.LegacyEffects
+// import indigoextras.effectmaterials.Refraction
+// import indigoextras.subsystems.FPSCounter
 
 final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, SandboxGameModel]:
 
   val gameId: GameId = GameId("sandbox-native")
 
   def initialScene(bootData: SandboxBootData): Option[SceneName] =
-    Some(WindowsScene.name)
+    // Some(WindowsScene.name)
+    None
 
   def scenes(bootData: SandboxBootData): NonEmptyBatch[Scene[SandboxStartupData, SandboxGameModel]] =
     ScenesList.scenes
@@ -25,73 +26,74 @@ final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, Sandbo
       BootResult(
         EngineConfig.default
           .withClearColor(RGBA(0.4, 0.2, 0.5, 1)),
-        SandboxBootData(args.find(_ == "key").getOrElse("No entry for 'key'."))
-      ).withAssets(
-        // TODO: For goodness sake, use the asset listing generator.
-        SandboxAssets.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/") ++
-          Shaders.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/") ++
-          Archetype.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/")
-      ).withFonts(
-        Fonts.fontInfo,
-        TestFont.fontInfo
-      ).withSubSystems(
-        FPSCounter[SandboxGameModel](
-          Fonts.fontKey,
-          SandboxAssets.smallFontName
-        )
-          .withLayerKey(Constants.LayerKeys.fps)
-          .placeAt { (context, bounds) =>
-            Point(0, context.frame.viewport.height - bounds.height)
-          }
-      ).withShaders(
-        Shaders.circle,
-        Shaders.external,
-        Shaders.sea,
-        LegacyEffects.entityShader,
-        Archetype.shader,
-        UVShaders.circle,
-        UVShaders.voronoi,
-        UVShaders.redBlend
-      ).addShaders(Refraction.shaders)
-        .addShaders(indigoextras.ui.shaders.all)
+        SandboxBootData() // args.find(_ == "key").getOrElse("No entry for 'key'."))
+      )
+      // .withAssets(
+      //   // TODO: For goodness sake, use the asset listing generator.
+      //   SandboxAssets.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/") ++
+      //     Shaders.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/") ++
+      //     Archetype.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/")
+      // ).withFonts(
+      //   Fonts.fontInfo,
+      //   TestFont.fontInfo
+      // ).withSubSystems(
+      //   FPSCounter[SandboxGameModel](
+      //     Fonts.fontKey,
+      //     SandboxAssets.smallFontName
+      //   )
+      //     .withLayerKey(Constants.LayerKeys.fps)
+      //     .placeAt { (context, bounds) =>
+      //       Point(0, context.frame.viewport.height - bounds.height)
+      //     }
+      // ).withShaders(
+      //   Shaders.circle,
+      //   Shaders.external,
+      //   Shaders.sea,
+      //   LegacyEffects.entityShader,
+      //   Archetype.shader,
+      //   UVShaders.circle,
+      //   UVShaders.voronoi,
+      //   UVShaders.redBlend
+      // ).addShaders(Refraction.shaders)
+      //   .addShaders(indigoextras.ui.shaders.all)
     )
 
   def setup(
       bootData: SandboxBootData,
       assetCollection: AssetCollection,
       dice: Dice
-  ): Outcome[Startup[SandboxStartupData]] = {
-    println(bootData.message)
+  ): Outcome[Startup[SandboxStartupData]] =
+    // println(bootData.message)
 
-    def makeStartupData(
-        aseprite: Aseprite,
-        spriteAndAnimations: SpriteAndAnimations,
-        clips: Map[CycleLabel, Clip[Material.Bitmap]]
-    ): Startup.Success[SandboxStartupData] =
-      Startup
-        .Success(
-          SandboxStartupData(
-            Dude(
-              aseprite,
-              spriteAndAnimations.sprite
-                .withRef(16, 16) // Initial offset, so when talk about his position it's the center of the sprite
-                .moveTo(SandboxGame.screenCenter) // Also place him in the middle of the screen initially
-                .withMaterial(SandboxAssets.dudeMaterial),
-              clips
-            )
-          )
-        )
-        .addAnimations(spriteAndAnimations.animations)
+    // def makeStartupData(
+    //     aseprite: Aseprite,
+    //     spriteAndAnimations: SpriteAndAnimations,
+    //     clips: Map[CycleLabel, Clip[Material.Bitmap]]
+    // ): Startup.Success[SandboxStartupData] =
+    //   Startup
+    //     .Success(
+    //       SandboxStartupData(
+    //         // Dude(
+    //         //   aseprite,
+    //         //   spriteAndAnimations.sprite
+    //         //     .withRef(16, 16) // Initial offset, so when talk about his position it's the center of the sprite
+    //         //     .moveTo(SandboxGame.screenCenter) // Also place him in the middle of the screen initially
+    //         //     .withMaterial(SandboxAssets.dudeMaterial),
+    //         //   clips
+    //         // )
+    //       )
+    //     )
+    //     .addAnimations(spriteAndAnimations.animations)
 
-    val res: Option[Startup.Success[SandboxStartupData]] = for {
-      json                <- assetCollection.findTextDataByName(AssetName(SandboxAssets.dudeName.toString + "-json"))
-      aseprite            <- Json.asepriteFromJson(json)
-      spriteAndAnimations <- aseprite.toSpriteAndAnimations(dice, SandboxAssets.dudeName)
-      clips               <- aseprite.toClips(SandboxAssets.dudeName)
-    } yield makeStartupData(aseprite, spriteAndAnimations, clips)
+    // val res: Option[Startup.Success[SandboxStartupData]] = for {
+    //   json                <- assetCollection.findTextDataByName(AssetName(SandboxAssets.dudeName.toString + "-json"))
+    //   aseprite            <- Json.asepriteFromJson(json)
+    //   spriteAndAnimations <- aseprite.toSpriteAndAnimations(dice, SandboxAssets.dudeName)
+    //   clips               <- aseprite.toClips(SandboxAssets.dudeName)
+    // } yield makeStartupData(aseprite, spriteAndAnimations, clips)
 
-    Outcome(res.getOrElse(Startup.Failure("Failed to load the dude")))
-  }
+    // Outcome(res.getOrElse(Startup.Failure("Failed to load the dude")))
+    Outcome(Startup.Success(SandboxStartupData()))
 
   def initialModel(startupData: SandboxStartupData): Outcome[SandboxGameModel] =
     Outcome(SandboxModel.initialModel(startupData))
@@ -100,23 +102,25 @@ final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, Sandbo
       context: Context[SandboxStartupData],
       model: SandboxGameModel
   ): GlobalEvent => Outcome[SandboxGameModel] =
-    SandboxModelShared.updateModel(context, model)
+    // SandboxModelShared.updateModel(context, model)
+    case _ => Outcome(model)
 
   def present(
       context: Context[SandboxStartupData],
       model: SandboxGameModel
   ): Outcome[SceneUpdateFragment] =
-    Outcome(
-      SceneUpdateFragment(
-        Constants.LayerKeys.background -> Layer.Stack.empty,
-        Constants.LayerKeys.game       -> Layer.Stack.empty,
-        Constants.LayerKeys.windows    -> Layer.Stack.empty,
-        Constants.LayerKeys.fps ->
-          Layer.empty
-            .withCamera(Camera.default)
-            .withMagnification(1)
-      )
-    )
+    Outcome(SceneUpdateFragment.empty)
+    // Outcome(
+    //   SceneUpdateFragment(
+    //     Constants.LayerKeys.background -> Layer.Stack.empty,
+    //     Constants.LayerKeys.game       -> Layer.Stack.empty,
+    //     Constants.LayerKeys.windows    -> Layer.Stack.empty,
+    //     Constants.LayerKeys.fps ->
+    //       Layer.empty
+    //         .withCamera(Camera.default)
+    //         .withMagnification(1)
+    //   )
+    // )
 
 object SandboxGame:
 
@@ -127,16 +131,16 @@ object SandboxGame:
   val screenCenter: Point =
     Point(viewportWidth, viewportHeight) / 2
 
-final case class Dude(
-    aseprite: Aseprite,
-    sprite: Sprite[Material.ImageEffects],
-    clips: Map[CycleLabel, Clip[Material.Bitmap]]
-)
-final case class SandboxBootData(message: String)
-final case class SandboxStartupData(dude: Dude)
-final case class SandboxViewModel(
-    offset: Point,
-    useLightingLayer: Boolean
-)
+// final case class Dude(
+//     aseprite: Aseprite,
+//     sprite: Sprite[Material.ImageEffects],
+//     clips: Map[CycleLabel, Clip[Material.Bitmap]]
+// )
+final case class SandboxBootData( /*message: String*/ )
+final case class SandboxStartupData( /*dude: Dude*/ )
+// final case class SandboxViewModel(
+//     // offset: Point,
+//     // useLightingLayer: Boolean
+// )
 
 final case class Log(message: String) extends GlobalEvent
--- indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/SandboxModel.scala
+++ indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/SandboxModel.scala
@@ -1,73 +1,75 @@
 package com.example.sandbox
 
-import com.example.sandbox.scenes.ActorPhysicsSceneModel
-import com.example.sandbox.scenes.ActorSceneModel
-import com.example.sandbox.scenes.CaptureScreenScene
-import com.example.sandbox.scenes.ComponentUIScene2
-import com.example.sandbox.scenes.ConfettiModel
-import com.example.sandbox.scenes.PathFindingModel
-import com.example.sandbox.scenes.PerformerPhysicsSceneModel
-import com.example.sandbox.scenes.PerformerSceneModel
-import com.example.sandbox.scenes.SfxComponents
-import indigo.*
-import indigo.syntax.*
-import indigoextras.mesh.*
-import indigoextras.ui.*
+// import com.example.sandbox.scenes.ActorPhysicsSceneModel
+// import com.example.sandbox.scenes.ActorSceneModel
+// import com.example.sandbox.scenes.CaptureScreenScene
+// import com.example.sandbox.scenes.ComponentUIScene2
+// import com.example.sandbox.scenes.ConfettiModel
+// import com.example.sandbox.scenes.PathFindingModel
+// import com.example.sandbox.scenes.PerformerPhysicsSceneModel
+// import com.example.sandbox.scenes.PerformerSceneModel
+// import com.example.sandbox.scenes.SfxComponents
+// import indigo.*
+// import indigo.syntax.*
+// import indigoextras.mesh.*
+// import indigoextras.ui.*
+import scala.annotation.nowarn
 
 final case class SandboxGameModel(
-    dude: DudeModel,
-    data: Option[String],
-    confetti: ConfettiModel,
-    pathfinding: PathFindingModel,
-    rotation: Radians,
-    num: Int,
-    sfxComponents: ComponentGroup[Unit],
-    components: ComponentGroup[Int],
-    scrollPane: ScrollPane[ComponentList[Int], Int],
-    button: Button[Int],
-    meshData: MeshData,
-    actorScene: ActorSceneModel,
-    actorPhysicsScene: ActorPhysicsSceneModel,
-    performerSceneModel: PerformerSceneModel,
-    performerPhysicsSceneModel: PerformerPhysicsSceneModel,
-    viewModel: SandboxViewModel,
-    captureScreenScene: CaptureScreenScene.Model,
-    viewportSize: Size
+    // dude: DudeModel,
+    // data: Option[String],
+    // confetti: ConfettiModel,
+    // pathfinding: PathFindingModel,
+    // rotation: Radians,
+    // num: Int,
+    // sfxComponents: ComponentGroup[Unit],
+    // components: ComponentGroup[Int],
+    // scrollPane: ScrollPane[ComponentList[Int], Int],
+    // button: Button[Int],
+    // meshData: MeshData,
+    // actorScene: ActorSceneModel,
+    // actorPhysicsScene: ActorPhysicsSceneModel,
+    // performerSceneModel: PerformerSceneModel,
+    // performerPhysicsSceneModel: PerformerPhysicsSceneModel,
+    // viewModel: SandboxViewModel,
+    // captureScreenScene: CaptureScreenScene.Model,
+    // viewportSize: Size
 )
 
 object SandboxModel:
 
+  @nowarn
   def initialModel(startupData: SandboxStartupData): SandboxGameModel =
-    val dice          = Dice.fromSeed(1)
-    val offset        = Point(75, 75)
-    val points        = List.fill(10)(SandboxModelShared.randomPoint(dice, offset)).toBatch
-    val superTriangle = Triangle.encompassing(points.map(_.toVertex), 10)
-    val mesh          = Mesh.fromVertices(points.map(_.toVertex), superTriangle)
+    // val dice          = Dice.fromSeed(1)
+    // val offset        = Point(75, 75)
+    // val points        = List.fill(10)(SandboxModelShared.randomPoint(dice, offset)).toBatch
+    // val superTriangle = Triangle.encompassing(points.map(_.toVertex), 10)
+    // val mesh          = Mesh.fromVertices(points.map(_.toVertex), superTriangle)
 
     SandboxGameModel(
-      DudeModel(startupData.dude, DudeIdle),
-      None,
-      ConfettiModel.empty,
-      PathFindingModel.empty,
-      Radians.zero,
-      0,
-      SfxComponents.components,
-      SandboxModelShared.components,
-      ComponentUIScene2.CustomComponents.pane,
-      SandboxModelShared.customButton,
-      MeshData(
-        points,
-        superTriangle,
-        mesh
-      ),
-      ActorSceneModel.initial,
-      ActorPhysicsSceneModel.initial,
-      PerformerSceneModel.initial,
-      PerformerPhysicsSceneModel.initial,
-      SandboxViewModel(
-        Point.zero,
-        true
-      ),
-      CaptureScreenScene.Model(None, None, Point.zero),
-      Size.one
+      // DudeModel(startupData.dude, DudeIdle),
+      // None,
+      // ConfettiModel.empty,
+      // PathFindingModel.empty,
+      // Radians.zero,
+      // 0,
+      // SfxComponents.components,
+      // SandboxModelShared.components,
+      // ComponentUIScene2.CustomComponents.pane,
+      // SandboxModelShared.customButton,
+      // MeshData(
+      //   points,
+      //   superTriangle,
+      //   mesh
+      // ),
+      // ActorSceneModel.initial,
+      // ActorPhysicsSceneModel.initial,
+      // PerformerSceneModel.initial,
+      // PerformerPhysicsSceneModel.initial,
+      // SandboxViewModel(
+      //   Point.zero,
+      //   true
+      // ),
+      // CaptureScreenScene.Model(None, None, Point.zero),
+      // Size.one
     )
